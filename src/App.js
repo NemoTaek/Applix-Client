9c59276c45db29ca2066fbed7eac6a0ebd67ac3f
pinroad <shirus87@gmail.com>
2020-11-10 20:23:50 +0900

    Completed : #7 mainpage setting-Router & basic CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
-function App() {
-  return <div className="App">Hello world</div>;
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+// pages
+import Main from "./pages/Main";
+// components
+import Nav from "./components/Nav";
+
+class App extends Component {
+  state = {
+    isLogin: false,
+  };
+
+  onLogin = () => {
+    this.setState((prevState) => ({
+      isLogin: !prevState.isLogin,
+    }));
+    console.log("로그인버튼state:", this.state);
+  };
+
+  render() {
+    const { isLogin } = this.state;
+
+    return (
+      <div className="wrap">
+        <header>
+          <Nav isLogin={isLogin} />
+        </header>
+        <div className="body"></div>
+        <Switch>
+          <Route path="/login">
+            {/* <Login isLogin={isLogin} onLogin={this.onLogin} /> */}
+          </Route>
+          <Route path="/mypage">{/* <Mypage /> */}</Route>
+          {/* <Route path="/signup" component={Signup} /> */}
+          <Route path="signup" />
+          <Route path="/findtheater" />
+          <Route path="/board" />
+          <Route path="/movielist" />
+          <Route exact path="/" component={Main} />
+        </Switch>
+        <footer></footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
+
+class Nav extends Component {
+  render() {
+    const { isLogin } = this.props;
+
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="selected">
+                배너 이미지(HOME)
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/findtheater" activeClassName="selected">
+                영화관찾기
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/board" activeClassName="selected">
+                모임게시글
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/movielist" activeClassName="selected">
+                영화목록
+              </NavLink>
+            </li>
+            {isLogin ? (
+              <>
+                <li>
+                  <NavLink to="/" activeClassName="selected">
+                    로그아웃
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/login" activeClassName="selected">
+                    로그인
+                  </NavLink>
+                </li>
+              </>
+            )}
+            {isLogin ? (
+              <>
+                <li>
+                  <NavLink to="/mypage" activeClassName="selected">
+                    마이페이지
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/signup" activeClassName="selected">
+                    회원가입
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav;
